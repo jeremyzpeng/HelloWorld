aad29c6ed073dd08d96d4b4ea3cd41cd62855935
jeremyzpeng <410327806@qq.com>
2015-06-30 17:59:47 +0800

    Initial commit

---
+++ app/src/main/java/MyActivity.java
@@ -0,0 +1,15 @@
+import android.app.Activity;
+import android.drm.DrmStore;
+import android.os.Bundle;
+
+/**
+ * Created by zhangpeng on 2015/6/26.
+ */
+
+public class MyActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a);
+    }
+}
---
+++ mytest/src/main/java/com/example/MyClass.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.util.List;
+
+public class MyClass {
+    public static void main(String args[]) {
+
+        system.out.println("´òÓ¡Êý¾Ý");
+    }
+}
